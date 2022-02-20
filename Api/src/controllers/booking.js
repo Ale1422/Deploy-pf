babff29753e245e8adffcce9d5828723648837e9
Ale1422 <alejandro.c.14.22@gmail.com>
2022-02-20 20:41:49 -0300

    accestpken

--- Api/src/controllers/booking.js
+++ Api/src/controllers/booking.js
@@ -19,7 +19,7 @@ const newBooking = async (req, res, next) => {
   const data= req.body
 
   try {    
-    const payData= await axios.get(`https://api.mercadopago.com/v1/payments/${data.data.id}/?access_token=${ACCESS_TOKEN}`).data
+    const payData= await axios.get(`https://api.mercadopago.com/v1/payments/${data.data.id}/?access_token=TEST-8344826949636961-021621-fa6f50dd49774c61c2de981dba9fbeae-157434994`).data
     // if(payData.status_detail === "accredited"){
     //       const [year,month,day,hour] = payData.aditional_info.items.description.split(',')
     //       const [external_reference, userId] = payData.external_reference.split('-')
